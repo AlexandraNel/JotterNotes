2424b73c10a2f8abc00ceea47d85995f343624ed
Lex <info@alexandranel.com>
2024-04-17 22:15:49 +1000

    confirm

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
+// Export a function we will use to PUT to the database.
 export const putDb = async (content) => {
   console.log('add content to the database');
 
